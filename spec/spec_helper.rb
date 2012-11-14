22e1bf2fc668349f8da938ba779c5eed314bb0bd
tchak <paul@chavard.net>
2012-11-14 15:17:20 +0100

    cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,3 @@ require "rspec/autorun"
 require "active_support/core_ext/numeric"
 
 require "rocha"
-# RSpec.configure do |config|
-#   config.include Konacha::RequestSpec, :type => :request
-# end
